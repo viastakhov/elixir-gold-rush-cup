e96eff2c242e5d6d29e9410c08b9369883207adc
Vladimir Astakhov <viastakhov@mail.ru>
2021-03-25 22:14:19 +0300

    the best solution at the moment

--- config/config.exs
+++ config/config.exs
@@ -8,13 +8,13 @@ config :gold_rush,
   field: %{width: 3500, height: 3500, depth: 10},
   licenses: %{max_licenses: 10, high_watermark: 9, interest_margin: 1, wallet_threshold: 25},
   pools: %{
-    explorers: %{size: 100, max_overflow: 0},
-    diggers: %{size: 10, max_overflow: 90},
+    explorers: %{size: 50, max_overflow: 0},
+    diggers: %{size: 10, max_overflow: 40},
     hackney: %{
       generic: %{timeout: 150000, max_connections: 10},
       licenser: %{timeout: 150000, max_connections: 100},
       accounter: %{timeout: 150000, max_connections: 100},
-      explorers: %{timeout: 150000, max_connections: 1000},
+      explorers: %{timeout: 150000, max_connections: 100},
       diggers: %{timeout: 150000, max_connections: 100}
     }
   }
